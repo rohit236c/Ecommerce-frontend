189a390271903ac37d7816ffdd439111fa9af034
rohit236c <rohit236c@gmail.com>
2020-01-02 16:00:11 +0530

    admin order page created
    Signed-off-by: rohit236c <rohit236c@gmail.com>

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -3,6 +3,7 @@ import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth/index';
 import {Link} from 'react-router-dom';
 import {listOrders} from './apiAdmin';
+import moment from 'moment';
 
 const Orders = () => {
     const [orders,
@@ -10,28 +11,101 @@ const Orders = () => {
     const {user, token} = isAuthenticated();
     const loadOrders = () => {
         listOrders(user._id, token).then(data => {
-            if (data.err) {} else {
-                setOrder({
-                    ...orders,
-                    orders: data
-                });
+            if (data.err) {
+                //err set
+
+            } else {
+                setOrder(data);
             }
         });
+
     };
-    const noOrders = (orders) => {
-        return orders && orders.length < 1
-            ? <h4>No Orders</h4>
-            : null;
-    }
     useEffect(() => {
         loadOrders();
-    }, [])
+    }, []);
+    const showOrdersLength = () => {
+        if (orders && orders.length > 0) {
+            console.log(orders, "ord")
+            return (
+                <h4 className="text-danger display-3">Total orders: {orders.length}</h4>
+            );
+        } else {
+            return (
+                <h4 className="text-danger">NO ORDERS</h4>
+            );
+        }
+    }
+    const showInput = (key, value) => (
+        <div className="input-group mb-2 mr-sm-2">
+            <div className="input-group-prepend mr-2">
+                <div className="input-group-text">
+                    {key}
+                </div>
+            </div>
+            <input type="text" value = {value} className="form-control" readOnly />
+        </div>
+    );
+
     return (
         <Layout title="Orders List" description={`Hello, ${user.name}!`}>
             <div className="row">
                 <div className="col-md-8 offset-md-2">
-                    {noOrders(orders)} 
-                    {JSON.stringify(orders)}    
+                    {showOrdersLength()}
+                    {orders.map((order, orderIndex) => {
+                        return (
+                            <div
+                                className="mt-5"
+                                key={orderIndex}
+                                style={{
+                                borderBottom: '5px solid indigo'
+                            }}>
+                                <h2 className="mb-5">
+                                    <span className="bg-primary">OrderId: {order._id}
+                                    </span>
+                                </h2>
+                                <ul className="list-group mb-2">
+                                    <li className="list-group-item">
+                                        Status: {order.status}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Transaction_id: {order.transaction_id}
+                                    </li>
+                                    <li className="list-group-item">
+                                        amount: Rs {order.amount}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Ordered By: {order.user.name}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Ordered On: {moment(order.createdAt).fromNow()}
+                                    </li>
+                                    <li className="list-group-item">
+                                        Delivery Address: {order.address}
+                                    </li>
+                                </ul>
+                                <h3 className="mt-4 mb-4 font-italic">
+                                    Total Products in the Order: {order.products && order.products.length}
+                                </h3>
+                                {order
+                                    .products
+                                    .map((p, pidx) => (
+                                        <div
+                                            className="mb-4"
+                                            key={pidx}
+                                            style
+                                            ={{
+                                            padding: '20px',
+                                            border: '1px solid indigo'
+                                        }}>
+                                            {showInput('Product Name', p.name)}
+                                            {showInput('Product Price', p.price)}
+                                            {showInput('Product Total', p.count)}
+                                            {showInput('Product Id', p._id)}
+                                        </div>
+                                    ))}
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </Layout>
